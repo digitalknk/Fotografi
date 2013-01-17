8161f8050ab254c7e6dc6aed380153322fc3b974
Jonathan Molina <jm@nerdla.bz>
2013-01-17 12:37:35 -0800

    Photos is not the root of the site. Photos also display on the index. Only logged user can see the upload, edit, and delete photos links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Fotografi::Application.routes.draw do
-	
+
+	root :to => "photos#index"
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   resources :users
   resources :sessions
   resources :photos
-  root :to => "home#index"
+  
 end
