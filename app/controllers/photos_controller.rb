a1f058ed0a822814ca9ae3f0ba1e68bad6a5b0b1
Jonathan Molina <jm@nerdla.bz>
2013-01-18 00:09:38 -0800

    Added some more user control. Added validation to make sure there was an image uploaded. Prettified the show.html file.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+  before_filter :require_login, :only => [:new, :create, :edit, :destroy]
+
   # GET /photos
   # GET /photos.json
   def index
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ActiveRecord::Base
   attr_accessible :image
   mount_uploader :image, ImageUploader
+
+  validates_presence_of :image
 end
