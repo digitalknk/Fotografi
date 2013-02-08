c045b064338c5b76253ec7d0f54fd322e99de7a5
Jonathan Molina <jm@nerdla.bz>
2013-02-07 16:03:27 -0800

    some asset juice for heroku.

--- config/application.rb
+++ config/application.rb
@@ -69,8 +69,11 @@ module Fotografi
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.logger = Logger.new(STDOUT)
   end
 end
